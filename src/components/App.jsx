b041ebe5a65255b0e1266e6323b9d40e02e1f4fe
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-12-04 10:52:02 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,60 @@
-import { Feedback } from './Feedback';
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
 export const App = () => {
-  return <Feedback />;
+  const feedbackOptions = [`good`, `neutral`, `bad`];
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = e => {
+    if (e.target.name === 'good') {
+      return setGood(good => good + 1);
+    }
+    if (e.target.name === 'neutral') {
+      return setNeutral(neutral => neutral + 1);
+    }
+    if (e.target.name === 'bad') {
+      return setBad(bad => bad + 1);
+    }
+  };
+
+  const totalFeedbacks = () => {
+    return good + neutral + bad;
+  };
+
+  const positivePercentage = () => {
+    if (totalFeedbacks() === 0) {
+      return 'No feedbacks';
+    }
+    return Math.round((good / totalFeedbacks()) * 100) + '%';
+  };
+
+  return (
+    <>
+      <Section title={`Please leave feedback`}>
+        <FeedbackOptions
+          options={feedbackOptions}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {totalFeedbacks !== 0 ? (
+          <Statistics
+            bad={bad}
+            good={good}
+            neutral={neutral}
+            total={totalFeedbacks()}
+            percentage={positivePercentage()}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,18 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Notification } from './Notification';
 
 const List = styled.ul`
   list-style: none;
 `;
 
 export const Statistics = ({ good, neutral, bad, total, percentage }) => {
-  return total === 0 ? (
-    <Notification text="There is no feedback" />
-  ) : (
+  return (
     <List>
       <li>Good: {good}</li>
       <li>Neutral: {neutral}</li>
       <li>Bad: {bad}</li>
       <li>Total: {total}</li>
-      <li>Positive feedback: {percentage}%</li>
+      <li>Positive feedback: {percentage}</li>
     </List>
   );
 };
@@ -25,5 +22,5 @@ Statistics.propTypes = {
   neutral: PropTypes.number,
   bad: PropTypes.number,
   total: PropTypes.number,
-  percentage: PropTypes.number,
+  percentage: PropTypes.string,
 };
